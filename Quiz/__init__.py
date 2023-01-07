0be7604f9fb62344b2d6dd5ccccd666644cb503d
Tony Stark <programmist0110@gmail.com>
2023-01-07 10:59:28 +0200

    initial init

---
+++ Quiz/__init__.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask
+import mysql.connector
+from mysql.connector import Error
+from flask_security import Security, SQLAlchemySessionUserDatastore
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config['SECRET_KEY'] = '01101997Programmer10?'
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PORT'] = 3306
+
+
+    app.config.from_mapping(SECRET_KEY = "dev", DATABASE = os.path.join(app.instance_path, 'flaskr'))
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+
+        try:
+            os.makedirs(app.instance_path)
+        except:
+            pass
+    
+    from . import db
+    db.init_app(app)
+
+    @app.route("/")
+    def hello():
+        return 'hello, world'
+    return app
---
+++ Quiz/db.py
@@ -0,0 +1,50 @@
+import mysql.connector
+from mysql.connector import Error
+from flask import current_app, g
+import click
+import email_validator
+
+
+
+def connect_db():
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="01101997Programmer10?",
+        database="mydb"
+    )
+
+    if not db.is_connected():
+        raise ConnectionError("could not connect to db")
+
+    db_info = db.get_server_info()
+    print("server: ", db_info)
+    cursor = db.cursor()
+    cursor.execute("SELECT DATABASE();")
+    database_name = cursor.fetchone()
+    print("Connected to: ", database_name)
+    return db
+
+
+def close_db(db):
+    connect_db().close
+
+
+def init_db():
+    db = connect_db()
+    with open("Quiz\schema.sql", 'r') as f:
+        with db.cursor() as cursor:
+            cursor.execute(f.read(), multi=True)
+            cursor.close()
+            db = connect_db()
+            cursor = db.cursor()
+        db.commit()
+
+@click.command('init-db')
+def init_db_command():
+    init_db()
+    click.echo("Initizing the database.")
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
